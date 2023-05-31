e059346ec7d5626208e5e9a201d3757fbcf86df7
Deena KC <dinakc14@gmail.com>
2023-05-30 20:58:31 -0400

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const heading =['Home', 'Services', 'Contact Us', 'Help']
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="header">
+      <span>{heading[0]} </span>
+      <span>{heading[1]}</span>
+      <span>{heading[2]}</span>
+      <span>{heading[3]}</span>
+     </div> 
+     <div className="body">
+      <button className="circle">Make an appontment</button>
+      <div className="secondbody">Get an amazing experience to relax, rewind and refresh with us!!!</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="footer"></div>
+
+
+      
     </>
   )
 }
-
 export default App
